eb656e96e05f05838ec91b722425a2422e4bf445
guyestguygithub001 <136700648+guyestguygithub001@users.noreply.github.com>
2023-08-20 14:34:11 +0100

    i.gets.cv Update gets_sizes.c

--- gets_sizes.c
+++ gets_sizes.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * get_size - Calculates the size to cast the argument
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
+ * get_size - Calculate size that print the argument.
+ * @format: Format strings that will print arguments.
+ * @i: The Listed arguments that are printed
  *
- * Return: Precision.
+ * Return: Exact Accuracy
  */
 int get_size(const char *format, int *i)
 {
